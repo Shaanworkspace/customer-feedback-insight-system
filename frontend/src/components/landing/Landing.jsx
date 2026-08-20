4d4d4fb6893368b33bc28460c995d7e43ee9208c
Shaanworkspace <shaanyworkspace@gmail.com>
2026-08-20 17:56:16 +0530

    Fix missing imports in Landing component

--- frontend/src/components/landing/Landing.jsx
+++ frontend/src/components/landing/Landing.jsx
@@ -1,3 +1,9 @@
+import Hero from './Hero'
+import WhyUs from './WhyUs'
+import Features from './Features'
+import HowItWorks from './HowItWorks'
+import StatsSection from './StatsSection'
+
 export default function Landing({ onStart }) {
   return (
     <main className="flex flex-1 flex-col">
